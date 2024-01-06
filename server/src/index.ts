aba29e0d0c44a7aa571fbf5ed490151d2ed15471
kunalmalik11 <kmsmkm2000@gmail.com>
2024-01-06 23:16:59 +0530

    login check

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,7 +2,8 @@ import express,{Request,Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import mongoose from 'mongoose';  
-import userRoutes from './routes/users'  
+import userRoutes from './routes/users' ;
+import authRoutes from './routes/auth';
 
 mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING as string)
 const app=express();
@@ -11,9 +12,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
 app.use("/api/users",userRoutes);
-app.get("/",(req:Request,res:Response)=>{
-    res.send(400);
-})
+app.use("/api/auth",authRoutes);
 
 app.listen(3000,()=>{
     console.log("server running on 5000")
