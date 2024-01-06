63a326408732ad9e44008fa201c9ada9654f729c
kunalmalik <kunal.malik@ekikrit.com>
2024-01-06 22:14:43 +0530

    user registration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,8 @@
 import express,{Request,Response} from 'express';
 import cors from 'cors';
 import "dotenv/config";
-import mongoose from 'mongoose';    
+import mongoose from 'mongoose';  
+import userRoutes from './routes/users'  
 
 mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING as string)
 const app=express();
@@ -9,10 +10,11 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 
-app.get("/api/test",async (req:Request,res:Response)=>{
-    res.json({message:"hello from express"});
-});
+app.use("/api/users",userRoutes);
+app.get("/",(req:Request,res:Response)=>{
+    res.send(400);
+})
 
 app.listen(3000,()=>{
-    console.log("server running on 3000")
+    console.log("server running on 5000")
 })
---
+++ server/src/routes/users.ts
@@ -0,0 +1,35 @@
+import express,{Request,Response} from "express";
+import User from "../models/user";
+import jwt from 'jsonwebtoken';
+
+const router=express.Router();
+
+router.post("/register",async (req:Request,res:Response)=>{
+    try{
+            let user=await User.findOne({
+                email:req.body.email,
+            });
+            if(user){
+                return res.status(400).json({
+                    message:"User already exists"
+                })
+            }
+            user =new User(req.body);
+            await user.save();
+            const token=jwt.sign({userId:user.id},process.env.JWT_SECRET_KEY as string,{
+                expiresIn:"1d"
+            });
+            res.cookie("auth_token",token,{
+                httpOnly:true,
+                secure:process.env.NODE_ENV==='prod',
+                maxAge:86400000,
+            });
+            return res.sendStatus(200);
+
+    }catch(error){
+            console.log(error);
+            res.status(500).send({message:"Something went wrong"});
+    }
+})
+
+export default router;
